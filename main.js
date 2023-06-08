94761822896d88edc138012fb1ae01bfcf82f99e
JuliusBarebones <123588625+JuliusBarebones@users.noreply.github.com>
2023-06-08 11:11:14 -0400

    Update main.js
    Update with other files.

--- main.js
+++ main.js
@@ -5,5 +5,34 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
-  });
-  
+});
+
+// Function to check if an element is in viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+
+    // Adjust this as per your requirement.
+    const halfwayThrough = elemTop < (window.innerHeight / 2) && elemBottom >= (window.innerHeight / 2);
+    
+    return isVisible || halfwayThrough;
+}
+
+// Listen for the scroll event
+window.addEventListener('scroll', function (e) {
+    // For each section
+    document.querySelectorAll('section').forEach(section => {
+        // If the section is in the viewport
+        if (isInViewport(section)) {
+            // Add the 'visible' class
+            section.classList.add('visible');
+        }
+    });
+});
